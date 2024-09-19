4a90a5b38834c18c507cc426dadaed734c037ba6
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-09-19 15:49:13 +0300

    Update

---
+++ PortfolioProject/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using PortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioDbEntities context = new MyPortfolioDbEntities();
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var values = context.Message.ToList();
+            return View(values);
+        }
+
+        public ActionResult MessageDetails(int id) {
+        var value = context.Message.Where(x=>x.MessageId == id).FirstOrDefault();
+            return View(value);
+        }
+
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value = context.Message.Where(x=> x.MessageId == id).FirstOrDefault();
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value = context.Message.Where(x => x.MessageId == id).FirstOrDefault();
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+    }
+}
---
+++ PortfolioProject/Controllers/SkillController.cs
@@ -0,0 +1,45 @@
+﻿using PortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    
+    public class SkillController : Controller
+    {
+        MyPortfolioDbEntities context = new MyPortfolioDbEntities();
+        // GET: Skill
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id) { 
+        var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+          
+        }
+
+
+    }
+}
